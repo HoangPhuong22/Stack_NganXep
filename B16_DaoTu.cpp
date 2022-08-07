142712605ca82ffa7a1c747ccfecacbafc23c701
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-08-07 21:29:31 +0700

    Update B16_DaoTu.cpp

--- B16_DaoTu.cpp
+++ B16_DaoTu.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    string x ; cin >> x;
+    string x ;
     stack<string>st;
     while(cin >> x)
     {
-    	st.push(x);
-	}
-	while(!st.empty())
-	{
-		cout << st.top() << " ";
-		st.pop();
-	}
+        st.push(x);
+    }
+    while(!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
     return 0;
 }
